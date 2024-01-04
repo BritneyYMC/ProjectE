755ffab6fa1a8b57858d9a7caddb7e3b1b775eae
mendo <mendo@192.168.1.234>
2024-01-03 21:22:45 -0500

    Initial Commit

---
+++ src/e/DemoE.java
@@ -0,0 +1,11 @@
+package e;
+
+public class DemoE {
+
+	public static void main(String[] args) {
+		System.out.println("Friday");
+		
+		System.out.println("HelloWorld");
+		System.out.println("Java");
+	}
+}
